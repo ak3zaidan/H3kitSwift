242521650de9fcfd061572dd27d37e835cebce02
ak3zaidan <74563626+ak3zaidan@users.noreply.github.com>
2024-01-05 20:20:46 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,14 +2,6 @@ import PackageDescription
 
 let package = Package(
   name: "H3kitSwift",
-  products: [
-    .library(name: "H3kitSwift", targets: ["H3kitSwift"])
-  ],
-  targets: [
-    .binaryTarget(
-      name: "H3kitSwift",
-      url: "https://github.com/ak3zaidan/H3kitSwift.git",
-      checksum: "15d896b7fd5dea5982594327bd2759a6ef6863a14d6aa6ffb72957f4064569ba"
-    )
-  ]
-)
+  platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
+  products: [.library(name: "H3kitSwift", targets: ["H3kitSwift"])],
+  targets: [.target(name: "H3kitSwift", path: "src")])
